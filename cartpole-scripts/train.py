f89eab49c943a57823d6c3ca31c94e688c25abff
devarashs <southernwavesofmadness@protonmail.com>
2025-04-30 12:42:47 +0330

    Add training and play scripts for CartPole using Stable Baselines3

--- cartpole-scripts/train.py
+++ cartpole-scripts/train.py
@@ -4,4 +4,4 @@ from stable_baselines3 import PPO
 env = gym.make("CartPole-v1")
 model = PPO("MlpPolicy", env, verbose=1)
 model.learn(total_timesteps=10000)
-model.save("ppo_cartpole")
+model.save("models/ppo_cartpole")
